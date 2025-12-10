577b327d3835dd0cd4c605b83d29a0007757088c
teni🐥 <120065486+pineapplegoose@users.noreply.github.com>
2025-12-10 23:46:40 +0100

    Refactor RootLayout to DashboardLayout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,22 +1,5 @@
 export const dynamic = "force-dynamic";
 
-import { ClerkProvider } from '@clerk/nextjs'
-import { ConvexsProviderWithClerk } from '../ConvexProviderWithClerk'
-import { AuthProvider } from '../../lib/AuthContext'
-
-
-export default function RootLayout({
-    children,
-}: {
-    children: React.ReactNode
-}) {
-    return (
-        <ClerkProvider>
-            <ConvexsProviderWithClerk>
-                <AuthProvider>
-                    {children}
-                </AuthProvider>
-            </ConvexsProviderWithClerk>
-        </ClerkProvider>
-    )
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
 }
