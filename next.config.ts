4a9d313912cac0613cb8f77cd8974dd7ef0d8409
teni🐥 <120065486+pineapplegoose@users.noreply.github.com>
2025-12-11 00:34:20 +0100

    Update next.config.ts for image handling
    Removed swcMinify option and updated image configuration.

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
-    domains: ["avatars.githubusercontent.com"],
-  },
-  experimental: {
-    serverActions: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
 }
 
